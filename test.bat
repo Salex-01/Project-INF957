43efa5740632e6e7c78771a7ea5cbaff7523adae
Sacha Guyot <salex01mail@gmail.com>
2021-05-03 02:27:51 -0400

    Scripts

---
+++ test.bat
@@ -0,0 +1,6 @@
+javaw -jar out/artifacts/AccountManager_jar/AccountManager.jar
+javaw -jar out/artifacts/FollowManager_jar/FollowManager.jar
+javaw -jar out/artifacts/MessageManager_jar/MessageManager.jar
+javaw -jar out/artifacts/CentralServer_jar/CentralServer.jar
+javaw -jar out/artifacts/ModuleManager_jar/ModuleManager.jar
+javaw -jar out/artifacts/Client_jar/Client.jar
---
+++ test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+java -jar out/artifacts/AccountManager_jar/AccountManager.jar
+java -jar out/artifacts/FollowManager_jar/FollowManager.jar
+java -jar out/artifacts/MessageManager_jar/MessageManager.jar
+java -jar out/artifacts/CentralServer_jar/CentralServer.jar
+java -jar out/artifacts/ModuleManager_jar/ModuleManager.jar
+java -jar out/artifacts/Client_jar/Client.jar
